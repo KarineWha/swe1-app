837ddff5ffd8482fd3fa474a7d9322d1db9a2974
KarineWha <kc5802@nyu.edu>
2026-02-23 14:25:56 -0500

    Include database with sample polls

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
+from .models import Question, Choice
 
-from .models import Question
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
